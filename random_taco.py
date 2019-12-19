629d165dcfae93473bb513aa16c40f77e6f2bf26
mv4847yh <mv4847yh@go.minnstate.edu>
2019-12-19 17:04:05 -0600

    The Taco Image

--- random_taco.py
+++ random_taco.py
@@ -1,7 +1,5 @@
 import docx  # import this library to create and update a word document
-from docx.shared import Inches
-from docx.enum.text import WD_BREAK
-from docx.enum.text import WD_BREAK # source: stack overFlow
+
 import requests # to get the url
 from PIL import Image, ImageDraw, ImageFont
 
@@ -18,13 +16,41 @@ font = ImageFont.truetype('DejaVuSans.ttf', 20)
 
 img_draw.text([200, 80], 'Random Taco Cookbook', fill='purple', font=font)
 # drawing text to the taco image
-Image.show()
 # showing the taco image
 Image.save('modified_taco.jpg')
 # saving the the taco image and naming it
 
+taco_book = []
+
+for i in range(3):
+    taco_url = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()
+    taco_book.append(taco_url)
+    print(taco_book)
+
 taco_word = docx.Document()
 
-taco_word.add_paragraoh('Random T..', 'Title')
+taco_word.add_paragraph('Random Taco Cookbook', 'Title')
 
 taco_word.add_picture('modified_taco.jpg')
+
+taco_word.add_paragraph("Tai's Captures")
+
+taco_word.add_paragraph('https://taco-1150.herokuapp.com/random/?full_taco=true')
+
+taco_word.add_paragraph('Mohamed Hussein')
+
+taco_word.add_page_break()
+
+for item in range(len(taco_book)):
+    taco_word.add_paragraph(taco_book[item]["seasoning"]["name"], 'heading 1')
+    taco_word.add_paragraph(taco_book[item]["seasoning"]["recipe"])
+    taco_word.add_paragraph(taco_book[item]["condiment"]["recipe"])
+    taco_word.add_paragraph(taco_book[item]["mixin"]["recipe"])
+    taco_word.add_paragraph(taco_book[item]["base_layer"]["recipe"])
+
+taco_word.save('Lo.docx')
+
+
+taco_word.add_page_break()
+
+taco_word.save('docx')
